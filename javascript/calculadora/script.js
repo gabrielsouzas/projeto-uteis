47ac101b3721f5c9b3e211720a6123fd3b26e522
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-06-29 16:04:38 -0300

    Adicionados os eventos cliques dos botões

--- javascript/calculadora/script.js
+++ javascript/calculadora/script.js
@@ -1,13 +1,46 @@
+const tela = document.querySelector('.tela');
 
 const criarEventos = () => {
     for (let index = 0; index < 20; index++) {
         const tecla = document.getElementsByClassName('tecla')[index];
-        tecla.addEventListener('click', calcular())
+        tecla.value = tecla.innerHTML;
+        tecla.addEventListener('click', calcular);
     }
 }
 
-const calcular = () => {
-    
+const calcular = ({ target }) => {
+    if (target.value === 'C') {
+        tela.innerHTML = '';
+    } else if (target.value === 'DEL') {
+        tela.innerHTML = tela.innerHTML.substring(0, tela.innerHTML.length - 1);
+    } else if (target.value === '+/-') {
+        
+    } else if (target.value === '=') {
+        
+    } else {
+        //!isNaN(target.value)
+        if (tela.innerHTML.length === 0) {
+            tela.innerHTML = target.value;
+        } else {
+            if (isNaN(tela.innerHTML.substr(-1))) {
+                if (!isNaN(target.value)) {
+                    execOpe(target);
+                }
+            } else {
+                execOpe(target);
+            }
+            
+        }
+    }
+}
+
+function execOpe(target) {
+    if (!isNaN(target.value)) {
+        tela.innerHTML += target.value;
+    } else {
+        var res = tela.innerHTML
+        
+    }
 }
 
 criarEventos();
