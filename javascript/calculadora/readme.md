5020b61cad0fd97a0a329a8d6593a18de67901e5
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-06-30 15:58:55 -0300

    Adicionadas novas funcionalidades
    Adicionadas as funcionalidades deletar, porcentagem, calculo com casas decimais, etc.

---
+++ javascript/calculadora/readme.md
@@ -0,0 +1,3 @@
+# Calculadora em Javascript
+
+Calculadora simples desenvolvida para praticar os meus conhecimentos adquiridos até o momento em Javascript, HTML5 e CSS3
--- javascript/calculadora/script.js
+++ javascript/calculadora/script.js
@@ -3,6 +3,8 @@ const tela = document.querySelector('.tela');
 var val1 = '';
 var op = '';
 var clicouIgual = false;
+var deletou = false;
+var segundoPonto = false;
 
 const criarEventos = () => {
     for (let index = 0; index < 20; index++) {
@@ -20,6 +22,8 @@ const calcular = ({ target }) => {
         clicouIgual = false;
     } else if (target.value === 'DEL') {
         tela.innerHTML = tela.innerHTML.substring(0, tela.innerHTML.length - 1);
+        val1 = tela.innerHTML;
+        //deletou = true;
     } else if (target.value === '+/-') {
         
     } else if (target.value === '=') {
@@ -42,35 +46,60 @@ const calcular = ({ target }) => {
 }
 
 function execOpe(target) {
-    if (!isNaN(target.value)) {
-        tela.innerHTML += target.value;
-    } else {
-        if (val1.length === 0) {
-            val1 = tela.innerHTML;
-            op = target.value;
-            tela.innerHTML += target.value;
-        } else {
-            if (clicouIgual) {
-                op = target.value;
+    if (!isNaN(target.value) || target.value === '.' || target.value === 'DEL') {
+        if (target.value === '.') {
+            if (!segundoPonto) {
                 tela.innerHTML += target.value;
-                clicouIgual = false;
-            } else {
-                tela.innerHTML = detecOperacao(val1, tela.innerHTML.split(op)[1], op);
+                segundoPonto = true;
+            }
+        } else {
+            tela.innerHTML += target.value;
+        }
+        
+        /*if (deletou){
+            val1 = tela.innerHTML;
+        }*/
+    } else {
+        segundoPonto = false;
+        //if (target.value != '.') {
+            if (val1.length === 0) {
                 val1 = tela.innerHTML;
                 op = target.value;
-                if (target.value != '=') {
+                tela.innerHTML += target.value;
+            } else {
+                //deletou = false;
+                //clicouPonto = false;
+                if (clicouIgual) {
+                    val1 = tela.innerHTML;
+                    op = target.value;
                     tela.innerHTML += target.value;
                     clicouIgual = false;
                 } else {
-                    clicouIgual = true;
+                    var res = detecOperacao(val1, tela.innerHTML.split(op)[1], op);
+                    if (!(res % 1 === 0)) {
+                        res = res.toFixed(2);
+                    }
+                    tela.innerHTML = res;
+                    val1 = tela.innerHTML;
+                    op = target.value;
+                    if (target.value != '=') {
+                        tela.innerHTML += target.value;
+                        clicouIgual = false;
+                    } else {
+                        clicouIgual = true;
+                    }
                 }
             }
-        }
+        /*} else {
+            tela.innerHTML += target.value;
+            clicouPonto = true;
+        }*/
 
     }
 }
 
 function detecOperacao(valor1, valor2, operador) {
+    console.log(valor1 + operador + valor2)
     if (operador === '/') {
         return Number(valor1) / Number(valor2);
     } else if (operador === 'X') {
@@ -80,7 +109,7 @@ function detecOperacao(valor1, valor2, operador) {
     } else if (operador === '+') {
         return Number(valor1) + Number(valor2);
     } else if (operador === '%') {
-        return Number(valor1) % Number(valor2);
+        return (Number(valor2) * Number(valor1))/100;
     }
 }
 
