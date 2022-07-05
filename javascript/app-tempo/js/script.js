4fde80a07ae523bff3c9b0bb78ffe92814b5e577
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-07-05 16:44:13 -0300

    Correções funcionamento

--- javascript/app-tempo/js/script.js
+++ javascript/app-tempo/js/script.js
@@ -6,18 +6,15 @@ const temperatureValue = document.querySelector('.card-temperature-value');
 const imgIcon = document.querySelector('.card-img-icon');
 const imgText = document.querySelector('.card-img-text');
 
-// Código para usar a API
-const KEY = 'e7129dfcf5e645d5991174633222406';
+const cityInput = document.querySelector('.city');
 
-// Uso do useState do Hooks (Dica: Use sempre o Hooks no inicio do código, nunca no meio)
-/* city: variavel, setCity: função de set da variavel
-Explicando: O useState vai armazenar a variavel city através do método setCity */
-var city = 'Assis';
+// Chave para usar a API
+const KEY = 'e7129dfcf5e645d5991174633222406';
 
 /* Dados iniciais */
 const initialData = {
     location: {
-      name: 'Assis',
+      name: '-',
       region: '-',
       country: '-',
     },
@@ -30,15 +27,13 @@ const initialData = {
     },
   };
 
-// useState para armazenar o retorno da API
+// Array para armazenar o retorno da API
 var data = initialData;
-
-/* Dados da API */
 
 // Essa função assincrona vai retornar uma promise, que é uma promessa do retorno,
 // ou seja, ela vai ficar aguardando o retorno dos dados
-const fetchData = async (city) => {
-  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+const fetchData = async () => {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${cityInput.value}&aqi=no`;
 
   /* Aqui a função fetch do JavasScript pega o retorno da chamada da url da API Weather */
   const fetchResponse = await fetch(url);
@@ -52,34 +47,26 @@ const fetchData = async (city) => {
 const handleSubmit = (event) => {
     event.preventDefault();
 
-    city = locationName.innerHTML;
     // O THEN vai esperar a execução da função
     // abaixo que vem de services/api por causa da promise da API
-    // Depois ele executa uma função anonima pasando a resposta da API e setando no useState data
-    fetchData(city).then((response) => {
-    data = response;
-    locationName.innerHTML = data.location.name;
-    locationInfo.innerHTML = `${data.location.region}, ${data.location.country}`;
-    temperatureValue.innerHTML = data.current.temp_c
-    imgText.innerHTML = data.current.condition.text
-    imgIcon.setAttribute('src', data.current.condition.icon);
+    // Depois ele executa uma função anonima
+    fetchData().then((response) => {
+    fillInFields(response);
     });
 };
+
+const fillInFields = (data) => {
+  locationName.innerHTML = data.location.name;
+  locationInfo.innerHTML = `${data.location.region}, ${data.location.country}`;
+  temperatureValue.innerHTML = data.current.temp_c
+  imgText.innerHTML = data.current.condition.text
+  imgIcon.setAttribute('src', data.current.condition.icon);
+}
 
 form.addEventListener('submit', handleSubmit);
 
 const load = () => {
-  city = 'Assis';
-  fetchData(city).then((response) => {
-   data = response; 
-  
-  locationName.innerHTML = data.location.name;
-    locationInfo.innerHTML = `${data.location.region}, ${data.location.country}`;
-    temperatureValue.innerHTML = data.current.temp_c
-    imgText.innerHTML = data.current.condition.text
-    imgIcon.setAttribute('src', data.current.condition.icon);
-  });
-
+    fillInFields(data);
 };
 
 load();
